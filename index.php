dc11757683ca35b81763d5faded943afc7285009
ted-miller92 <ted.miller92@gmail.com>
2021-09-28 13:29:28 -0700

    Commit for tues Sep 28

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-// Main index file
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href="css/styles.css" type="text/css" rel="stylesheet"/>
+<title>My Portal Page</title>
+
+</head>
+
+<body>
+</body>
+</html>
