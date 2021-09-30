eacb6cd544f8f32ae5f1b040d8ac06f4966f9853
ted-miller92 <ted.miller92@gmail.com>
2021-09-29 22:49:21 -0700

    started portal.php

--- index.php
+++ index.php
@@ -4,10 +4,85 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link href="css/styles.css" type="text/css" rel="stylesheet"/>
-<title>My Portal Page</title>
+<title>Ted Miller Portal Page</title>
 
 </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="">Switch</a></li>
+                        <li><a href="">Troubleshooting</a></li>
+                        <li><a href="">Calculator</a></li>
+                        <li><a href="">Another Assignment</a></li>
+                        <li><a href="">And Another</a></li>
+                    </ul>
+                </nav>
+            </header>
 
-<body>
-</body>
+            <main>
+                <h1>Ted's IT 261 Portal</h1>
+                <p>I am currently learning web development, and I'm slowly discovering all the keyboard shortcuts that make it faster and easier. Most of my adult life has been spent behind a saute pan with a set of tongs, coming home smelling like onions and pasta water.</p>
+                <p>Since I started school in Fall of 2020, I have learned HTML, CSS, Javascript, and some Python, and I've become comfortable using Linux as an OS. It's been enlightening and kind of empowering to learn about the internet and how it functions, and its place in our world.</p>
+            </main>
+
+            <aside>
+                <h3>Weekly Exercises</h3>
+                <h5>Week 2</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 3</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 4</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 5</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 6</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 7</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                <h5>Week 8</h5>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+            </aside>
+
+            <footer>
+                <ul>
+                    <li>Copyright &copy; 2021</li>
+                    <li>All Rights Reserved</li>
+                    <li><a href="../index.php">Web Design by Ted Miller</a></li>
+                    <li><a href="">Terms of Use</a></li>
+                    <li><a href="">HTML Validation</a></li>
+                    <li><a href="">CSS Validation</a></li>
+                </ul>
+            </footer>
+        </div><!--end wrapper -->
+    </body>
 </html>
