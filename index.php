f137faea62354eb35979792d5a460cef7bd492d3
ted-miller92 <ted.miller92@gmail.com>
2021-09-30 22:52:16 -0700

    styled portal page, needs colors

--- index.php
+++ index.php
@@ -8,23 +8,41 @@
 
 </head>
     <body>
-        <div id="wrapper">
-            <header>
-                <nav>
-                    <ul>
-                        <li><a href="">Switch</a></li>
-                        <li><a href="">Troubleshooting</a></li>
-                        <li><a href="">Calculator</a></li>
-                        <li><a href="">Another Assignment</a></li>
-                        <li><a href="">And Another</a></li>
-                    </ul>
-                </nav>
-            </header>
+        <header>
+            <h2>Ted Miller</h2>
+            <nav>
+                <ul>
+                    <li><a href="">Switch</a></li>
+                    <li><a href="">Troubleshooting</a></li>
+                    <li><a href="">Calculator</a></li>
+                    <li><a href="">Another Assignment</a></li>
+                    <li><a href="">And Another</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="hero">
+            <div id="heroLeft">
+                <div id="circlePhotoWrapper">
+                    <img id="tedAndBird" src="./images/IMG_0103.jpg" alt="Ted with bird">    
+                </div><!--end circlePhotoWrapper-->
+                    
+            </div><!--end heroLeft-->
 
+            <div id="heroRight">
+                <div id="bio">
+                    <h1>Ted's IT 261 Portal</h1>
+                        <p>I am currently learning web development, and I'm slowly discovering all the keyboard shortcuts that make it faster and easier. Most of my adult life has been spent behind a saute pan with a set of tongs, coming home smelling like onions and pasta water.</p>
+                        <p>Since I started school in Fall of 2020, I have learned HTML, CSS, Javascript, and some Python, and I've become comfortable using Linux as an OS. It's been enlightening and kind of empowering to learn about the internet and how it functions, and its place in our world.</p>
+                    </div><!--end heroRight-->
+                </div><!--end bio-->
+                
+            </div> <!--end hero-->
+        <div id="wrapper">
             <main>
-                <h1>Ted's IT 261 Portal</h1>
-                <p>I am currently learning web development, and I'm slowly discovering all the keyboard shortcuts that make it faster and easier. Most of my adult life has been spent behind a saute pan with a set of tongs, coming home smelling like onions and pasta water.</p>
-                <p>Since I started school in Fall of 2020, I have learned HTML, CSS, Javascript, and some Python, and I've become comfortable using Linux as an OS. It's been enlightening and kind of empowering to learn about the internet and how it functions, and its place in our world.</p>
+                <h2>MAMP Portal</h2>
+                <img id="mampPortal" class="screenshot" src="./images/mampWorking.png" alt="mamp working">
+                <h2>PHP Errors Displaying</h2>
+                <img id="phpError" class="screenshot" src="./images/phpErrorMessage.png" alt="error message">
             </main>
 
             <aside>
@@ -74,14 +92,13 @@
             </aside>
 
             <footer>
-                <ul>
-                    <li>Copyright &copy; 2021</li>
-                    <li>All Rights Reserved</li>
-                    <li><a href="../index.php">Web Design by Ted Miller</a></li>
-                    <li><a href="">Terms of Use</a></li>
-                    <li><a href="">HTML Validation</a></li>
-                    <li><a href="">CSS Validation</a></li>
-                </ul>
+                <p>
+                    Copyright &copy; 2021 All Rights Reserved
+                    <a href="../index.php">Web Design by Ted Miller</a>
+                    <a href="">Terms of Use</a>
+                    <a href="">HTML Validation</a>
+                    <a href="">CSS Validation</a>
+                </p>
             </footer>
         </div><!--end wrapper -->
     </body>
