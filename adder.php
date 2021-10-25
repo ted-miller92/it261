6ef7487f58f95c05a281b1f63c5bf554ae796934
ted-miller92 <ted.miller92@gmail.com>
2021-10-24 21:01:08 -0700

    fixed "non-numeric" error

--- adder.php
+++ adder.php
@@ -17,8 +17,8 @@
     <?php //adder-wrong.php
 
         if (isset($_POST['num1'])){
-            $num1 = $_POST['num1'];
-            $num2 = $_POST['num2'];
+            $num1 = intval($_POST['num1']);
+            $num2 = intval($_POST['num2']);
             $myTotal = $num1 + $num2;
             echo '<h2>You added '.$num1.' and '.$num2.'</h2>';
             echo '<p style="color:red;">and the answer is<br>'.$myTotal.'!</p>';
