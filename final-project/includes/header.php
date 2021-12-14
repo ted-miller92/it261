4bf6740e59c1f2f272a42dd03e501a5406ccbc55
ted-miller92 <ted.miller92@gmail.com>
2021-12-13 22:42:05 -0800

    removed redundant directories

--- final-project/includes/header.php
+++
@@ -1,9 +0,0 @@
-<?php include('config.php');
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-        
