26ba67d29e7b219d909ebb290e1270d981c1ab41
ted-miller92 <ted.miller92@gmail.com>
2021-12-14 14:10:15 -0800

    landing page

--- final/index.php
+++ final/index.php
@@ -3,10 +3,27 @@
     <main>
         <h3>Welcome to Movie Club</h3>
         <p>This is a club where you can sign up to talk about your favorite movies, discuss new releases and learn about old classics.</p>
+        <div class="gridContainer">
+            <div class="orange">
+                <a href="movies.php">
+                    <h5>Explore your favorite movies</h5>
+                    <img src="images/movie_list_screenshot.png" alt="movies screenshot">
+                </a>
+            </div>
+            <div class="purpleOrange">
+                <a href="movies.php">
+                    <h5>See the Director of the Day</h5>
+                    <img src="images/directors_screenshot.png" alt="movies screenshot">
+                </a>
+            </div>
+            <div class="purple">
+                <a href="movies.php">
+                    <h5>Contact us!</h5>
+                    <img src="images/contact_screenshot.png" alt="movies screenshot">
+                </a>
+            </div>
+        </div><!-- end gridContainer -->
     </main>
-    <aside>
-    
-    </aside>
 </div><!-- end wrapper -->
 <?php include('includes/footer.php'); ?>
 <!-- <body> closes in footer.php -->
